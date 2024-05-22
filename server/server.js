032adc825448c4aa95cb0091fe8c89531f7dfb73
Sandhya015 <sandhyanaik3249@gmail.com>
2024-05-22 22:52:37 +0530

    update2

--- server/server.js
+++ server/server.js
@@ -2,17 +2,8 @@ const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
 
-app.use("/api/auth",router);
+app.use("/api/auth",router)
 
-// app.get("/",(req,res) => {
-
-//     res.status(200).send("Hello World");
-// });
-
-// app.get("/register",(req,res) => {
-
-//     res.status(200).send("Welcome To the  registration page");
-// })
 const PORT=5000;
 app.listen(PORT,()=>{
     console.log(`Server is running on port: ${PORT}`);
