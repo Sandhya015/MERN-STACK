d8a4495a9e81483378a2aba334730984f74ba8a6
Sandhya015 <sandhyanaik3249@gmail.com>
2024-05-22 22:52:46 +0530

    new

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/",(req,res) => {
+router.route("/").get((req,res) => {
 
     res.status(200).send("Hello World my dear");
 });
 
-// app.get("/register",(req,res) => {
+router.route("/register").get((req,res) => {
 
-//     res.status(200).send("Welcome To the  registration page");
-// })
-
+        res.status(200).send("Welcome To the  registration page");
+    })
 
 module.exports = router;
