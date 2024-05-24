6dbfdd484dd5ca707cf8101c9097498ecf84fb24
Sandhya015 <sandhyanaik3249@gmail.com>
2024-05-24 23:59:05 +0530

    first

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -9,10 +9,11 @@ const home = async(req,res)=>{
 
 const register = async(req,res)=>{
     try{
-        res.status(200).send("welcome to the registration page");
+        console.log(req.body);
+        res.status(200).json({ message:req.body});
         
     }catch(error){
-        res.status(400).send({msg:"page not found"});
+        res.status(500).json("internal server error");
     }
 };
 module.exports = {home,register};
