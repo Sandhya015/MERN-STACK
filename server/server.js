0c01505433650b1b6c3eaa0576a7b28fa1c6d311
Sandhya015 <sandhyanaik3249@gmail.com>
2024-05-26 22:00:06 +0530

    db connection successful

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
-
+const connectDb = require("./utils/db");
 app.use(express.json());
 
 app.use("/api/auth", router);
 
 const PORT = 5000;
+
+connectDb().then(() =>{
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
+});
 });
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,3 +1,17 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(URI);
+const URI ="mongodb://127.0.0.1:27017/mern_admin";
+
+// mongoose.connect(URI);
+
+const connectDb = async() =>{
+try{
+    await mongoose.connect(URI);
+    console.log("connection suuccessful to db");
+
+}catch(error){
+    console.log("datbase failed to connect");
+    process.exit(0);
+}
+}
+module.exports =connectDb;
