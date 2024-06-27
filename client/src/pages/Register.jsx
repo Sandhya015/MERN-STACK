2be7cd7eaf456111a3cee86fc41e614e5ef8b0dc
Sandhya015 <sandhyanaik3249@gmail.com>
2024-06-27 21:48:34 +0530

    sssssss

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,3 +1,21 @@
 export const Register = ()=> {
-    return <h1>Hello Homepage</h1>
+    return <>
+    <section>
+        <main>
+            <div className="secction-registration">
+                <div className="container grid grid-two-cols">
+                 <div className="registration-image">
+                    <img src="/images/i1.jpeg" alt="" 
+                     width="500"
+                     height="400"
+                    />
+
+                 </div>
+
+
+              </div>
+            </div>
+        </main>
+    </section>
+    </>;
 }; 
